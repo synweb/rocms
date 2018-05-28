e74f0ebcee1de3a7234a9005253c6cf4d3d25396
stoyana <stoyana@bk.ru>
2018-05-28 13:24:17 +0300

    Фикс удаления товара, который приобртали

--- Shop/RoCMS.Shop.Database/Tables/GoodsInOrder.sql
+++ Shop/RoCMS.Shop.Database/Tables/GoodsInOrder.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [Shop].[GoodsInOrder] (
     [Quantity] INT NOT NULL,
-    [HeartId]  INT            NOT NULL,
+    [HeartId]  INT            NULL,
     [OrderId]  INT            NOT NULL,
     [PackId] INT NULL, 
     [Id] INT NOT NULL IDENTITY, 
     [Price] DECIMAL(18, 2) NOT NULL DEFAULT 0, 
     CONSTRAINT [PK_GoodsInOrderSet] PRIMARY KEY CLUSTERED ([Id]),
-    CONSTRAINT [FK_GoodsInOrderGoods] FOREIGN KEY ([HeartId]) REFERENCES [Shop].[GoodsItem] ([HeartId]),
+    CONSTRAINT [FK_GoodsInOrderGoods] FOREIGN KEY ([HeartId]) REFERENCES [Shop].[GoodsItem] ([HeartId]) ON DELETE SET NULL, 
     CONSTRAINT [FK_GoodsInOrderOrder] FOREIGN KEY ([OrderId]) REFERENCES [Shop].[Order] ([OrderId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_GoodsInOrderPack] FOREIGN KEY ([PackId]) REFERENCES [Shop].[Pack] ([PackId]) ON DELETE SET NULL, 
     CONSTRAINT [AK_GoodsInOrderSet_Column] UNIQUE ([HeartId], [OrderId], [PackId])

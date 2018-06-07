d0b6500b5e4a6a610e872086e32c15adac5b1456
stoyana <stoyana@bk.ru>
2018-06-07 15:59:44 +0300

    Строгое вхождение в поиске Наверх

--- RoCMS.Database/dbo/Stored Procedures/SearchItem_FindStrict.sql
+++ RoCMS.Database/dbo/Stored Procedures/SearchItem_FindStrict.sql
@@ -2,7 +2,7 @@
 	@SearchQuery nvarchar(2000),
 	@Entities String_Table readonly
 AS
-	SELECT s.SearchItemKey, s.[EntityName], s.[EntityId], s.[Text], s.Title, s.[Url], s.[Weight], s.[ImageId], s.[HeartId], 0 as [Rank]
+	SELECT s.SearchItemKey, s.[EntityName], s.[EntityId], s.[Text], s.Title, s.[Url], s.[Weight], s.[ImageId], s.[HeartId], 1000000 as [Rank]
 		FROM [SearchItem] s 
 		WHERE
 			(NOT EXISTS (SELECT * FROM @Entities) OR (s.EntityName IN (SELECT Val FROM @Entities)))

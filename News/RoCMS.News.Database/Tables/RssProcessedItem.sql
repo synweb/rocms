801a6b3e84e4275c356974f571ad0a3584787e30
Seed122 <srgsemenoff@gmail.com>
2018-11-22 09:14:28 +0300

    Forgotten 'on delete set null'!

--- News/RoCMS.News.Database/Tables/RssProcessedItem.sql
+++ News/RoCMS.News.Database/Tables/RssProcessedItem.sql
@@ -4,5 +4,5 @@
 	[RssSource] nvarchar(4000) NOT NULL UNIQUE,
 	[CreationDate] datetime not null default GETUTCDATE(),
 	[NewsItemId] int null, 
-    CONSTRAINT [FK_RssProcessedItems_NewsItem] FOREIGN KEY ([NewsItemId]) REFERENCES [News].[NewsItem]([HeartId])
+    CONSTRAINT [FK_RssProcessedItems_NewsItem] FOREIGN KEY ([NewsItemId]) REFERENCES [News].[NewsItem]([HeartId]) ON DELETE SET NULL
 )

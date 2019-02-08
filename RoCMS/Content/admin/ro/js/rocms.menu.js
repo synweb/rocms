9cde246ac633c24faede54f671b7e8a99d1a15bd
umka <filkova@synweb.ru>
2019-02-08 19:14:12 +0300

    Фикс проблемы с меню: при сортировке пунктов не применялся плагин на выпадающий список страниц

--- RoCMS/Content/admin/ro/js/rocms.menu.js
+++ RoCMS/Content/admin/ro/js/rocms.menu.js
@@ -35,6 +35,8 @@ App.Admin.MenuItem = function () {
 
         parent.items.remove(item);
         parent.items.splice(index - 1, 0, item);
+
+        $(".withsearch").selectpicker();
     };
     
     self.moveDown = function (item, parent) {
@@ -43,6 +45,8 @@ App.Admin.MenuItem = function () {
 
         parent.items.remove(item);
         parent.items.splice(index + 1, 0, item);
+
+        $(".withsearch").selectpicker();
     };
 
     self.remove = function (item, parent) {
